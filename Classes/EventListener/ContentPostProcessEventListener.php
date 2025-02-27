2bb574354f11e863582bb83cab28ae410cdd62c6
Mathis Koblin <mathis.koblin@elementare-teilchen.de>
2025-02-27 10:30:53 +0000

    Finished migration from Hook to EventListener

--- Classes/EventListener/ContentPostProcessEventListener.php
+++ Classes/EventListener/ContentPostProcessEventListener.php
@@ -2,15 +2,21 @@
 
 declare(strict_types=1);
 
-namespace IchHabRecht\SocialGdpr\Hooks;
+namespace IchHabRecht\SocialGdpr\EventListener;
 
 use IchHabRecht\SocialGdpr\Handler\ContentMatch;
 use IchHabRecht\SocialGdpr\Handler\HandlerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Attribute\AsEventListener;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\CMS\Frontend\Event\AfterCacheableContentIsGeneratedEvent;
 
-class ContentPostProcessHook
+#[AsEventListener(
+    identifier: 'socialgdprAfterCacheableContentIsGeneratedEventListener',
+)]
+final readonly class ContentPostProcessEventListener
 {
     /**
      * @var ContentObjectRenderer|null
@@ -22,17 +28,12 @@ class ContentPostProcessHook
         $this->contentObjectRenderer = GeneralUtility::makeInstance(ContentObjectRenderer::class);
     }
 
-    public function replaceSocialMediaWithEvent(\TYPO3\CMS\Frontend\Event\AfterCacheableContentIsGeneratedEvent $event): void
+    public function __invoke(AfterCacheableContentIsGeneratedEvent $event): void
     {
-        $this->replaceSocialMediaInContent($event->getController());
+        $this->replaceSocialMediaInContent($event->getController(), $event->getRequest());
     }
 
-    public function replaceSocialMedia(array $parameter): void
-    {
-        $this->replaceSocialMediaInContent($parameter['pObj']);
-    }
-
-    protected function replaceSocialMediaInContent(TypoScriptFrontendController $typoScriptFrontendController)
+    protected function replaceSocialMediaInContent(TypoScriptFrontendController $typoScriptFrontendController, ServerRequestInterface $request): void
     {
         $content = $typoScriptFrontendController->content;
 
@@ -63,7 +64,7 @@ class ContentPostProcessHook
                     );
 
                     $this->contentObjectRenderer->start($data, 'tt_content');
-                    $typoScript = $this->contentObjectRenderer->getRequest()->getAttribute('frontend.typoscript')->getSetupArray();
+                    $typoScript = $request->getAttribute('frontend.typoscript')->getSetupArray();
                     $handlerContent = $this->contentObjectRenderer->cObjGetSingle($typoScript['lib.']['socialgdpr'], $typoScript['lib.']['socialgdpr.']);
                     $content = str_replace($match->getSearch(), $handlerContent, $content);
                 }
